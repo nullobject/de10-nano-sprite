0218a9abff548ee562583d63519475b2251c732f
Joshua Bassett <hello@joshbassett.info>
2019-08-10 18:34:04 +1000

    Fix issue with sprite flipping

--- src/sprite_blitter.vhd
+++ src/sprite_blitter.vhd
@@ -176,9 +176,9 @@ begin
 
   -- set destination position and handle X/Y axis flipping
   dest_pos.x <= resize(sprite.pos.x+src_pos.x, dest_pos.x'length) when sprite.flip_x = '0' else
-                resize(sprite.pos.x-src_pos.x+sprite.size, dest_pos.x'length);
+                resize(sprite.pos.x-src_pos.x+sprite_size-1, dest_pos.x'length);
   dest_pos.y <= resize(sprite.pos.y+src_pos.y, dest_pos.y'length) when sprite.flip_y = '0' else
-                resize(sprite.pos.y-src_pos.y+sprite.size, dest_pos.y'length);
+                resize(sprite.pos.y-src_pos.y+sprite_size-1, dest_pos.y'length);
 
   -- the pre-blit is done when the first two pixels have been loaded
   preload_done <= '1' when state = PRELOAD and load_pos.x = 1 else '0';
